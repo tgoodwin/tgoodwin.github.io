64bcc4f31774fc7eb593ea8427610d8d8716f16d
Timothy <tlg2132@columbia.edu>
2015-07-17 13:09:29 -0400

    changem

--- vector/vector.js
+++ vector/vector.js
@@ -4,8 +4,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   var res = 20;
-  var countX = ceil(width/res) + 1;
-  var countY = ceil(height/res) + 1;
+  var countX = ceil(width/res) + 1; //x positions
+  var countY = ceil(height/res) + 1; //y positions
 
   for (var j = 0; j < countY; j++) {
     for (var i = 0; i < countX; i++) {
@@ -14,18 +14,18 @@ function setup() {
   };
 
   noFill();
-  stroke(249,78,128);
+  stroke(128,128,128);
 }
 
 function draw() {
-  background(30,67,137);
+  background(30,30,30);
   for (var i = locs.length - 1; i >= 0; i--) {
     var h = calcVec( locs[i].x - mouseX, locs[i].y - mouseY);
     line(
       locs[i].x,
       locs[i].y,
-      locs[i].x + 15*cos(h.heading()),
-      locs[i].y + 15*sin(h.heading())
+      locs[i].x + 10*cos(h.heading()),
+      locs[i].y + 10*sin(h.heading())
     );
   };
 }
